7078b347c03c9d3843f9753629f1ba110cbd67c2
바이트 케이크 <bytecakelake@gmail.com>
2023-11-22 23:40:25 +0900

    fix

--- server/common/config.py
+++ server/common/config.py
@@ -2,7 +2,7 @@
 adress = "127.0.0.1" 
 port = 5000
 debug = False
-openai_api_key_path = "common\opnai_api_key.txt"
+openai_api_key_path = "common\openai_api_key.txt"
 base_prompt_path = "nickname_prompts/prompt-1.txt"
 
 
