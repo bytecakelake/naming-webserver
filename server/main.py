945adbea7052503107c05971e003976cf7e28e3b
바이트 케이크 <bytecakelake@gmail.com>
2023-11-22 23:31:28 +0900

    update

--- server/main.py
+++ server/main.py
@@ -3,22 +3,36 @@ from pydantic import BaseModel
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import base64
-import json
+from openai import OpenAI
+from common import config
 
-def chatgpt(key: str, prompt: str):
-    from openai import OpenAI
-    client = OpenAI(api_key=key)
+def nickname_generatior(key: str, user_prompt: str):
 
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-                    {"role": "system", "content": "You are a helpful assistant."},
-                    {"role": "user", "content": prompt},
-                ]
-        )
+    response = llm.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+        "role": "system",
+        "content": config.base_prompt
+        },
+        {
+        "role": "assistant",
+        "content": "어떤 닉네임을 생성해 드릴까요?"
+        },
+        {
+        "role": "user",
+        "content": user_prompt
+        },
+    ],
+    temperature=1.5,
+    max_tokens=100,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
     return response
 
+llm = OpenAI(api_key=config.openai_api_key)
 app = FastAPI()
 
 
@@ -29,12 +43,11 @@ templates = Jinja2Templates(directory=f"{webui_path}/template")
 
 
 @app.get('/api', response_class=JSONResponse)
-async def generator_API(requset: Request):
-    print(requset.query_params)
-    return 
+async def generate(request: Request, user_prompt: str):
+    return nickname_generatior(config.openai_api_key, user_prompt)
 
 @app.get('/', response_class=HTMLResponse)
-async def root(request: Request, pompt: str | None):
+async def root(request: Request):
     return templates.TemplateResponse('index.html', {"request": request, "client_ip": request.client.host})
 
 
