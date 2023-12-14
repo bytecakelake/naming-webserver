920722a10c1ad106511691c94726eb6be20d6318
Lee Jonghyun <bytcakelake@gmail.com>
2023-12-14 07:50:58 +0000

    commit

--- server/database/webui/static/main.js
+++ server/database/webui/static/main.js
@@ -1,16 +1,25 @@
-async function generateNickname() {
-    const userPrompt = document.getElementById('user_prompt').value;
+async function naming() {
+    const userPrompt = document.getElementById('input').value;
     if (!userPrompt.trim()) {
-        document.querySelector('.chat #status').innerText = '경고: 입력이 비어있습니다.';
         return;
     }
-    document.querySelector('.chat #status').style.display = 'block';
-    document.querySelector('.chat #status').innerText = '잠시만 기다려주세요...';
-    document.querySelector('.chat #output.box').innerText = `닉네임 생성중...`;
-    const response = await fetch(`/api/generate?mode=nickname&prompt=${userPrompt}`);
+    document.querySelector('#status').style.backgroundColor = '#ee7e00';
+    document.querySelector('#status').innerText = `닉네임 생성중...`;
+    const language = `ko`;
+    const response = await fetch(`/api/generate/${language}/name`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            {
+                "prompt": userPrompt,
+            }
+        ),
+    });
     const data = await response.json();
-    document.querySelector('.chat #status').innerText = `닉네임 생성을 완료했습니다.`;
-    document.querySelector('.chat #output.box').style.height = '10rem';
-    document.querySelector('.chat #output.box').innerHTML = `<p>Nick-Id:${data.uuid}</p><p>닉네임:${data.nickname}</p><p>설명:${data.explanation}</p>`;
-
+    document.querySelector('#status').style.backgroundColor = '#009000';
+    document.querySelector('#status').innerText = `닉네임 생성을 완료했습니다.`;
+    document.querySelector('#name').innerText = data.name;
+    document.querySelector('#description').innerText = data.description;
 }
--- server/router/api.py
+++ server/router/api.py
@@ -1,65 +1,32 @@
 from fastapi import APIRouter, Request, Response
 from fastapi.responses import JSONResponse
 from uuid import uuid1, UUID
-from common import config
 import pandas as pd
-import numpy as np
-from openai import OpenAI
+from core.bot.naming import Nicus
+from pydantic import BaseModel
 
 
-
+bot = Nicus()
 router = APIRouter(prefix="/api", default_response_class=JSONResponse, tags=["api"])
-openai = OpenAI(api_key=config.openai_api_key)
 
-class Logs:
-    generated = pd.DataFrame(columns=['prompt', 'nickname', 'explanation'], dtype=np.dtype('<U'))
+class Log:
+    name = pd.DataFrame(columns=['prompt', 'name', 'description'])
 
+class GetNameModel(BaseModel):
+    prompt: str
 
-async def nickname_generator(user_prompt: str):
-    response = openai.chat.completions.create(
-        model="gpt-3.5-turbo-0613",
-        messages=[
-            {"role": "system", "content": config.sysetem_prompt},
-            { "role": "assistant","content": config.assistant_prompt},
-            {"role": "user",  "content": user_prompt},
-        ],
-        temperature=1.2,
-        max_tokens=config.ans_limit,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    content = response.choices[0].message.content.replace("\n\n", "\n").replace(": ", ":").split("\n")
-    if len(content) != 2:
-        print(" 비정상적인 생성기 응답감지. 다시 시도를 권장합니다.")
-        print(content)
-        return Response(status_code=500)
-    nickname = content[1].split(":")[-1]
-    if "(" in nickname and ")" in nickname:
-        print("닉네임에 부가설명이 감지되어 제거하였습니다.")
-        nickname = nickname.split("(")[0]
-    explanation = content[0].split(":")[1]
-    return {"nickname": nickname, "explanation": explanation}
-
-@router.get('/generate', status_code=202)
-async def generator_gate(request: Request, mode: str, prompt: str):
-    
-    if prompt == "":
-        return Response(status_code=400)
-    if mode == "nickname":
-        content = await nickname_generator(prompt)
-        #content = {"nickname": "테스트", "explanation": "테스트"}
-
-        uuid = uuid1()
-        Logs.generated.loc[uuid] = {"nickname": content["nickname"], "explanation": content["explanation"], "prompt": prompt}
-        return {"uuid": uuid, "nickname": content["nickname"], "explanation": content["explanation"]}
-    return Response(status_code=400)
-
-@router.get('/search')
-async def search(request: Request, uuid: UUID,):
-    try:
-        return Logs.generated.loc[uuid]
-    except KeyError:
+@router.post('/generate/{language}/name', status_code=202)
+async def naming(language:str, item: GetNameModel):
+    if language not in ['en', 'ko']:
         return Response(status_code=404)
+    if language == 'en':
+        language = 'english'
+    elif language == 'ko':
+        language = 'Korean'
+    content = bot.generate(item.prompt, language)['content']
+    content['uuid'] = str(uuid1())
+    content['prompt'] = item.prompt
+    return content
+
 
    
